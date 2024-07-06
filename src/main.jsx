ce3e6994b79ca307650055862dc336c654d354ac
pranmandale <pranavmandale2025@gmail.com>
2024-07-06 12:38:24 +0530

    new files added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,84 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+import store from './store/Store.js';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import Protected from './components/AuthLayout.jsx';
+import Signup from './pages/Signup.jsx';
+import AllPosts from './pages/AllPost.jsx';
+import AddPost from './pages/AddPost.jsx';
+import EditPost from './pages/EditPost.jsx';
+import Post from './pages/Post.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: (
+          <Protected authentication={false}>
+            <Login />
+          </Protected>
+        )
+      },
+      {
+        path: '/signup',
+        element: (
+          <Protected authentication={false}>
+            <Signup />
+          </Protected>
+        )
+      },
+      {
+        path: '/all-posts',
+        element: (
+          <Protected authentication={true}>
+            <AllPosts />
+          </Protected>
+        )
+      },
+      {
+        path: '/add-post',
+        element: (
+          <Protected authentication={true}>
+            <AddPost />
+          </Protected>
+        )
+      },
+      {
+        path: '/edit-post/:slug',
+        element: (
+          <Protected authentication={true}>
+            <EditPost />
+          </Protected>
+        )
+      },
+      {
+        path: '/post/:slug',
+        element: (
+          <Protected authentication={true}>
+            <Post />
+          </Protected>
+        )
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
